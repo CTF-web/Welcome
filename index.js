cadf285120d328f433dca67ccae26cb828c7e37e
CTF-web <kvimukthi48@gmail.com>
2026-02-11 09:10:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+ clue3: {
+    title: "The GitHub Secret",
+    description: "Congratulations! You decoded the GitHub link.",
+    hint: "The final flag is hidden in the response headers. Check the X-CTF-Flag header!"
+  }
